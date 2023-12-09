d916c641b48978047f11e6ea5778ec636cadaedb
Bahar Keshavarz <bahar.keshavarzc@gmail.com>
2023-12-09 11:37:02 +0330

    add themeprovider to code

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,6 +3,7 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <>
+      <div>applicatin1</div>
       <div className="h1-bold">
         applicatin
         <UserButton afterSignOutUrl="/" />
