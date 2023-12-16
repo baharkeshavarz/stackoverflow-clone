10fc81c86ec1c5724039a6176d003dace32d26ee
Bahar Keshavarz <bahar.keshavarzc@gmail.com>
2023-12-16 22:52:54 +0330

    add rightsidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "@/components/shared/navbar/Navbar";
 import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
 
 interface layoutProps {
   children: React.ReactNode;
@@ -15,7 +16,7 @@ const Layout = ({ children }: layoutProps) => {
         <section className="max-md:pb-14 flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
-        RightSidebar
+        <RightSidebar />
       </div>
       Toaster
     </main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -56,9 +56,14 @@ const config: Config = {
       "auth-light": "url('/assets/images/auth-light.png)",
     },
     screens: {
-      xs: "420px",
-      sm: "640px",
-      lg: "1024px",
+      sm: "576px",
+      // => @media (min-width: 576px) { ... }
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+      lg: "992px",
+      // => @media (min-width: 992px) { ... }
+      xl: "1200px",
+      // => @media (min-width: 1200px) { ... }
     },
     keyframes: {
       "accordion-down": {
