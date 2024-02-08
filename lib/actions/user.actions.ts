612b07c36ca764ffb1b54745979ab49a684674ed
Bahar Keshavarz <bahar.keshavarzc@gmail.com>
2024-02-08 17:44:01 +0330

    add webhooks funcs

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,7 +1,14 @@
 "use server";
 
+import {
+  CreateUserParams,
+  DeleteUserParams,
+  UpdateUserParams,
+} from "@/types/shared.types";
 import db from "../db";
 import User from "@/database/user.model";
+import { revalidatePath } from "next/cache";
+import Question from "@/database/question.model";
 
 export async function getUserById(params: any) {
   try {
@@ -15,5 +22,63 @@ export async function getUserById(params: any) {
     return user;
   } catch (error) {
     console.log("error");
+    throw error;
+  }
+}
+
+export async function createUser(userData: CreateUserParams) {
+  try {
+    db.connect();
+
+    const newUser = await User.create(userData);
+    return newUser;
+  } catch (error) {
+    console.log("error");
+    throw error;
+  }
+}
+
+export async function updateUser(params: UpdateUserParams) {
+  try {
+    db.connect();
+
+    const { clerkId, updateData, path } = params;
+    await User.findOneAndUpdate({ clerkId }, updateData, {
+      new: true,
+    });
+    revalidatePath(path);
+  } catch (error) {
+    console.log("error");
+    throw error;
+  }
+}
+
+export async function deleteUser(params: DeleteUserParams) {
+  try {
+    db.connect();
+
+    const { clerkId } = params;
+    const user = await User.findOneAndDelete({ clerkId });
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    // delete all questions, answers, comments, etc.
+
+    // get user quesions ids
+    // const userQuestionIds = await Question.find({ author: user._id }).distinct(
+    //   "_id"
+    // );
+
+    // delete user questions
+    await Question.deleteMany({ author: user._id });
+
+    // TODO: delete user answers, comments and .etc
+
+    const deleteUser = await User.findByIdAndDelete(user._id);
+    return deleteUser;
+  } catch (error) {
+    console.log("error");
+    throw error;
   }
 }
