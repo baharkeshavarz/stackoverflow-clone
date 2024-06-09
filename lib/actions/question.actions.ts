f89bc3c333fcfdcc60ecd9096e507410eaadb36b
Bahar Keshavarz <bahar.keshavarzc@gmail.com>
2024-06-09 23:29:47 +0330

    make the filtering working

--- lib/actions/question.actions.ts
+++ lib/actions/question.actions.ts
@@ -20,7 +20,7 @@ import { FilterQuery } from "mongoose";
 export async function getQuestions(params: GetQuestionParams) {
   try {
     await db.connect();
-    const { searchQuery } = params;
+    const { searchQuery, filter } = params;
     const query: FilterQuery<typeof Question> = {};
 
     if (searchQuery) {
@@ -30,10 +30,26 @@ export async function getQuestions(params: GetQuestionParams) {
       ];
     }
 
+    // Filters
+    let sortOptions = {};
+    switch (filter) {
+      case "newest":
+        sortOptions = { createdAt: -1 };
+        break;
+      case "frequent":
+        sortOptions = { views: -1 };
+        break;
+      case "unanswered":
+        query.answers = { $size: 0 };
+        break;
+      default:
+        break;
+    }
+
     const questions = await Question.find(query)
       .populate({ path: "tags", model: Tag })
       .populate({ path: "author", model: User })
-      .sort({ createdAt: -1 });
+      .sort(sortOptions);
 
     return { questions };
   } catch (error) {
