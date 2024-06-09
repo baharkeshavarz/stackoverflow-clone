756254630ae1af065ec68f48dc6f162c9d3aa78d
Bahar Keshavarz <bahar.keshavarzc@gmail.com>
2024-06-09 23:19:35 +0330

    working on filter in home page

--- components/home/HomeFilters.tsx
+++ components/home/HomeFilters.tsx
@@ -1,10 +1,35 @@
 "use client";
 import { HomePageFilters } from "@/constants/filters";
-import React from "react";
+import { useState } from "react";
 import { Button } from "../ui/button";
+import { useSearchParams, useRouter } from "next/navigation";
+import { formUrlQuery } from "@/lib/utils";
 
 const HomeFilters = () => {
-  const isActive = "newest";
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const [active, setActive] = useState("");
+
+  const handleTypeClick = (item: string) => {
+    if (active === item) {
+      setActive("");
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: "",
+      });
+      router.push(newUrl, { scroll: false });
+    } else {
+      setActive(item);
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: item.toLowerCase(),
+      });
+      router.push(newUrl, { scroll: false });
+    }
+  };
+
   return (
     <div className="mt-10 hidden w-full flex-wrap gap-3 md:flex">
       {HomePageFilters.map((filter) => (
@@ -12,11 +37,11 @@ const HomeFilters = () => {
           key={filter.value}
           className={`body-medium rounded-lg px-6 py-2 capitalize shadow-none
           ${
-            isActive === filter.value
+            active === filter.value
               ? "bg-primary-100 text-primary-500"
               : "bg-light-800 text-light-500"
           }`}
-          onClick={() => {}}
+          onClick={() => handleTypeClick(filter.value)}
         >
           {filter.name}
         </Button>
