1b7988513f65164c665abb4c8cefeec97f71a0fb
Bahar Keshavarz <bahar.keshavarzc@gmail.com>
2024-06-14 17:23:31 +0330

    start working on functionality of global search

--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -1,24 +1,68 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { Input } from "@/components/ui/input";
+import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
+import GlobalResult from "./GlobalResult";
 
 const GlobalSearch = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const query = searchParams.get("q");
+  const [search, setSearch] = useState(query || "");
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (search) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "global",
+          value: search,
+        });
+        router.push(newUrl, { scroll: false });
+      } else {
+        if (query) {
+          const newUrl = removeKeysFromQuery({
+            params: searchParams.toString(),
+            keysToRemove: ["global", "type"],
+          });
+          router.push(newUrl, { scroll: false });
+        }
+      }
+    }, 300);
+    return () => clearTimeout(delayDebounceFn);
+  }, [search, searchParams, pathname, router]);
+
   return (
-    <div className="background-light800_dark400 relative flex min-h-[56px] w-full max-w-[600px] items-center justify-center gap-1 rounded-xl px-4">
-      <Image
-        src="/assets/icons/search.svg"
-        width={24}
-        height={24}
-        alt="search"
-        className="cursor-pointer"
-      />
-      <Input
-        type="text"
-        placeholder="Search globally"
-        className="no-focus placeholder paragraph-regular background-light800_dark400 border-none shadow-none outline-none"
-      />
+    <div className="relative w-full max-w-[600px]">
+      <div className="background-light800_dark400 flex min-h-[56px] w-full items-center justify-center gap-1 rounded-xl px-4">
+        <Image
+          src="/assets/icons/search.svg"
+          width={24}
+          height={24}
+          alt="search"
+          className="cursor-pointer"
+        />
+        <Input
+          type="text"
+          placeholder="Search globally"
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value);
+            if (e.target.value === "" && isOpen) {
+              setIsOpen(false);
+            } else if (!isOpen) {
+              setIsOpen(true);
+            }
+          }}
+          className="no-focus placeholder paragraph-regular background-light800_dark400 border-none shadow-none outline-none"
+        />
+      </div>
+      {isOpen && <GlobalResult />}
     </div>
   );
 };
