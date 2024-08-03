e7ba3338fdded3d1ab42ccf01d9c6a90f173f599
Bahar Keshavarz <bahar.keshavarzc@gmail.com>
2024-08-03 07:43:57 -0230

    add loading to community

---
+++ app/(root)/(home)/community/loading.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Skeleton } from "@/components/ui/skeleton";
+import React from "react";
+
+const Loading = () => {
+  return (
+    <section>
+      <h1 className="h1-bold text-dark100_light900">All Users</h1>
+      <div className="mb-12 mt-11 flex flex-wrap gap-5">
+        <Skeleton className="h-14 flex-1 bg-slate-100" />
+        <Skeleton className="h-14 w-28 bg-slate-100" />
+      </div>
+
+      <div className="flex flex-wrap gap-4">
+        {[1, 2, 3, 4, 5, 6].map((item) => (
+          <Skeleton
+            key={item}
+            className="h-60 w-full rounded-2xl bg-slate-100 sm:w-[230px]"
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Loading;
